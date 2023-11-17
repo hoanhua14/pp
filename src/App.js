12d73c4c96b300c7dda8b6612c4e4b450cba3396
hoanhua14 <huasohoan@gmail.com>
2023-11-16 20:53:42 -0800

    working on parallax effect

--- src/App.js
+++ src/App.js
@@ -3,17 +3,35 @@ import NavBar from './Nav';
 import Projects from './Projects';
 import TechStacks from './TechStacks';
 import './styling/App.css';
+import skyImage from './sky.jpg';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 function App() {
   return (
-    <>
+    <div>
+      <div><NavBar/></div>
+      <Parallax pages={4}>
 
-      <NavBar/>
-      <div className='center-container'>
-        <AboutMe/>
-        <TechStacks/>
-        <Projects/>
-      </div>
-    </>
+        <ParallaxLayer offset={0} speed={1} factor={1}
+          style={{
+            backgroundImage: `url(${skyImage})`,
+            backgroundSize: 'cover',
+          }}>
+          <div>About me</div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1}>
+          <div>Tech Stacks</div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2}>
+          <div>Projects</div>
+        </ParallaxLayer>
+
+        {/* <div className='center-container'>
+          <AboutMe/>
+          <TechStacks/>
+          <Projects/>
+        </div> */}
+      </Parallax>
+    </div>
   );
 }
 
