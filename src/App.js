234f96c401cd8930748de310dafa7fe19df0c994
hoanhua14 <huasohoan@gmail.com>
2023-11-29 19:28:16 -0800

    changed bg

--- src/App.js
+++ src/App.js
@@ -3,25 +3,33 @@ import NavBar from './Nav';
 import Projects from './Projects';
 import TechStacks from './TechStacks';
 import './styling/App.css';
-import skyImage from './sky.jpg';
+import pinkBg from './pinkBg.jpg'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 function App() {
   return (
     <div>
-      <div><NavBar/></div>
+      {/* <div><NavBar/></div> */}
       <Parallax pages={4}>
 
         <ParallaxLayer offset={0} speed={1} factor={1}
           style={{
-            backgroundImage: `url(${skyImage})`,
+            backgroundImage: `url(${pinkBg})`,
             backgroundSize: 'cover',
           }}>
           <div>About me</div>
         </ParallaxLayer>
-        <ParallaxLayer offset={1}>
+        <ParallaxLayer offset={1}
+          style={{
+            backgroundImage: `url(${pinkBg})`,
+            // backgroundSize: 'cover',
+          }}>
           <div>Tech Stacks</div>
         </ParallaxLayer>
-        <ParallaxLayer offset={2}>
+        <ParallaxLayer offset={2}
+          style={{
+            backgroundImage: `url(${pinkBg})`,
+            backgroundSize: 'cover',
+          }}>
           <div>Projects</div>
         </ParallaxLayer>
 
