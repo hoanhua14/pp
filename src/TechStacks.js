03ad7994c95392c68c796a774e4879da4c64c246
hoanhua14 <huasohoan@gmail.com>
2024-01-29 23:18:01 -0800

    icon change

--- src/TechStacks.js
+++ src/TechStacks.js
@@ -1,11 +1,18 @@
 import git from './techStacks/git.svg'
 import js from './techStacks/js.svg'
+import github from './techStacks/github.svg'
+import python from './techStacks/python.svg'
+import typescript from './techStacks/typescript.svg'
+
 const TechStacks = () => {
 
     return (
         <div>techstacks
            <img alt='git' src={git}/>
+           <img alt='github' src={github}/>
            <img alt='js' src={js}/>
+           <img alt='typescript' src={typescript}/>
+           <img alt='python' src={python}/>
         </div>
     );
 };
