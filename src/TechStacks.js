a90ff85b0b75b10712c82cf13f7c16b13f5e696c
hoanhua14 <huasohoan@gmail.com>
2024-01-30 20:47:55 -0800

    techstacks

--- src/TechStacks.js
+++ src/TechStacks.js
@@ -3,17 +3,44 @@ import js from './techStacks/js.svg'
 import github from './techStacks/github.svg'
 import python from './techStacks/python.svg'
 import typescript from './techStacks/typescript.svg'
+import react from './techStacks/react.svg'
+import html from './techStacks/html.svg'
+import css from './techStacks/css.svg'
+import tailwind from './techStacks/tailwind.svg'
+import node from './techStacks/node.svg'
+import django from './techStacks/django.svg'
+import postgresql from './techStacks/postgresql.svg'
+import mongodb from './techStacks/mongodb.svg'
+import api from './techStacks/api.svg'
+import sql from './techStacks/sql.svg'
+
 
 const TechStacks = () => {
 
     return (
-        <div>techstacks
-           <img alt='git' src={git}/>
-           <img alt='github' src={github}/>
-           <img alt='js' src={js}/>
-           <img alt='typescript' src={typescript}/>
-           <img alt='python' src={python}/>
-        </div>
+        <>
+            <div>frontend
+
+                <img className='img' alt='git' src={git}/>
+                <img className='img' alt='github' src={github}/>
+                <img className='img' alt='js' src={js}/>
+                <img className='img' alt='typescript' src={typescript}/>
+                <img className='img' alt='python' src={python}/>
+                <img className='img' alt='react' src={react}/>
+                <img className='img' alt='html' src={html}/>
+                <img className='img' alt='css' src={css}/>
+                <img className='img' alt='tailwind' src={tailwind}/>
+            </div>
+            <div>backend
+                <img className='img' alt='node' src={node}/>
+                <img className='img' alt='django' src={django}/>
+                <img className='img' alt='postgresql' src={postgresql}/>
+                <img className='img' alt='mongodb' src={mongodb}/>
+                <img className='img' alt='api' src={api}/>
+                <img className='img' alt='sql' src={sql}/>
+
+            </div>
+        </>
     );
 };
 export default TechStacks;
